687c010f4881cd19e064f98324c48d4aacf5ed8d
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2019-12-29 15:33:42 +0000

    save baseline prediction

--- add_distance_to_injury.R
+++ add_distance_to_injury.R
@@ -233,17 +233,16 @@ if(file.exists(paste0(overflow_path,'processed_injuries_8.Rds'))){
   saveRDS(injury_table,paste0(overflow_path,'processed_injuries_8.Rds'),version=2)
 }
 
+## save for prediction and compute model #############################################
+
+pred_year <- 2015
 # get city data
 city_table <- injury_table
 for(i in 1:2)
   for(j in 1:2)
-    city_table[[i]][[j]] <- injury_table[[i]][[j]][injury_table[[i]][[j]]$year==2015,]
+    city_table[[i]][[j]] <- injury_table[[i]][[j]][injury_table[[i]][[j]]$year==pred_year,]
 
-saveRDS(city_table,'../mh-execute/inputs/injury/processed_injuries_9.Rds',version=2)
-
-
-######################################################################
-## model
+## model ######################################################################
 
 trim_glm_object <- function(obj){
   obj$y <- c()
@@ -300,11 +299,14 @@ if(test_model){
   predict(mod1)
 }
 
+basic_formula <- 'count~year+cas_severity+cas_mode*strike_mode+road+road:(cas_mode+strike_mode)+region+cas_mode:cas_severity+strike_mode:cas_severity'
 
+
+## original model: for distance and exponents not uncertain ###############################################
 #formula_one <- 'count~ns(year,df=2)+cas_severity+cas_mode+strike_mode+road+region+offset(log(cas_distance)+log(strike_distance))'
 CAS_EXPONENT <- 1
 STR_EXPONENT <- 1
-formula_one <- 'count~year+cas_severity+cas_mode*strike_mode+road+road:(cas_mode+strike_mode)+region+offset(log(cas_distance)+log(strike_distance)+(CAS_EXPONENT-1)*log(cas_distance_sum)+(STR_EXPONENT-1)*log(strike_distance_sum))'
+formula_one <- paste0(basic_formula,'+offset(log(cas_distance)+log(strike_distance)+(CAS_EXPONENT-1)*log(cas_distance_sum)+(STR_EXPONENT-1)*log(strike_distance_sum))')
 
 ##!! decide offset, splines, interactions
 
@@ -322,7 +324,6 @@ for(i in 1:2) {
       form <- paste0(form,'+strike_male*ns(strike_age,df=4)+strike_mode:(strike_male+ns(strike_age,df=4))')
       #injury_table[[i]][[j]] <- injury_table[[i]][[j]][order(strike_male,strike_age),]
     }
-    form <- paste0(form,'+cas_mode:cas_severity')
     print(form)
     ##for model build, set rate=1
     injury_table[[i]][[j]]$rate <- 1
@@ -331,11 +332,12 @@ for(i in 1:2) {
     trimmed_mod <- trim_glm_object(mod[[i]][[j]])
     trimmed_mod$offset <- c()
     trimmed_mod$linear.predictors <- c()
+    city_table[[i]][[j]]$base_pred <- predict(trimmed_mod,newdata=city_table[[i]][[j]],type='response')
     saveRDS(trimmed_mod,paste0('../mh-execute/inputs/injury/city_region',i,j,'.Rds'),version=2)
   }
 }
 
-
+saveRDS(city_table,'../mh-execute/inputs/injury/processed_injuries_9.Rds',version=2)
 
 common_coefs <- intersect(names(mod[[1]][[1]]$coefficients),names(mod[[2]][[2]]$coefficients))
 do.call(cbind,lapply(mod,function(x)sapply(x,function(y)y$coefficients[names(y$coefficients)%in%common_coefs])))
@@ -343,33 +345,4 @@ cas_coefs <- names(mod[[1]][[1]]$coefficients)[!names(mod[[1]][[1]]$coefficients
 sapply(mod[[1]],function(y)y$coefficients[names(y$coefficients)%in%cas_coefs])
 str_coefs <- names(mod[[1]][[1]]$coefficients)[!names(mod[[1]][[1]]$coefficients)%in%names(mod[[1]][[2]]$coefficients)]
 sapply(list(mod[[1]][[1]],mod[[2]][[1]]),function(y)y$coefficients[names(y$coefficients)%in%str_coefs])
-
-mod <- list()
-for(i in 1:2) {
-  mod[[i]] <- list()
-  for(j in 1:2) {
-    form <- formula_one
-    if(i==1) form <- paste0(form,'+cas_male*ns(cas_age,df=4)+(cas_male+ns(cas_age,df=4)):cas_mode')
-    if(j==1) form <- paste0(form,'+strike_male*ns(strike_age,df=4)+(strike_male+ns(strike_age,df=4)):strike_mode')
-    form <- paste0(form,'+strike_mode:cas_severity')
-    print(form)
-    ##for model build, set rate=1
-    injury_table[[i]][[j]]$rate <- 1
-    mod[[i]][[j]] <- glm(as.formula(form),family=poisson(link=log),data=injury_table[[i]][[j]])
-    print(AIC(mod[[i]][[j]]))
-    #saveRDS(mod[[i]][[j]],paste0('/scratch/rob/city_region',i,j,'.Rds'),version=2)
-    trimmed_mod <- trim_glm_object(mod[[i]][[j]])
-    #print(sapply(mod[[i]][[j]],function(x)length(serialize(x, NULL))))
-    #print(1)
-    #predict(trimmed_mod,newdata=injury_table[[i]][[j]],type='response')
-    trimmed_mod$offset <- c()
-    #print(2)
-    #predict(trimmed_mod,newdata=injury_table[[i]][[j]],type='response')
-    trimmed_mod$linear.predictors <- c()
-    #print(3)
-    #predict(trimmed_mod,newdata=injury_table[[i]][[j]],type='response')
-    saveRDS(trimmed_mod,paste0('../mh-execute/inputs/injury/city_region',i,j,'.Rds'),version=2)
-  }
-}
-
 
